172a8cf18cf60b21ce000b2255208e018d29e861
danile71 <danile71@gmail.com>
2018-10-21 10:42:10 +0300

    init repo

---
+++ drivers/misc/mediatek/mach/mt6795/irmn6795_a55_64/touchpanel/max1187x/tpd_custom_max1187x.h
@@ -0,0 +1,9 @@
+#ifndef __TPD_CUSTOM_MAX1187X_H__
+#define __TPD_CUSTOM_MAX1187X_H__
+
+#include <mach/mt_pm_ldo.h>
+
+#define TPD_I2C_NUMBER 2
+#define TPD_POWER_SOURCE_CUSTOM MT6331_POWER_LDO_VGP1
+
+#endif
