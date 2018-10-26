5c43c305234d284e3830eceacfa01e18589c999f
danile71 <danile71@gmail.com>
2018-10-26 19:01:18 +0300

    [PATCH] patch: Linux 3.10.107

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -179,7 +179,7 @@ static int xhci_start(struct xhci_hcd *xhci)
 				"waited %u microseconds.\n",
 				XHCI_MAX_HALT_USEC);
 	if (!ret)
-		xhci->xhc_state &= ~XHCI_STATE_HALTED;
+		xhci->xhc_state = 0;
 
 	return ret;
 }
